366ae105556e430ca1465bb2a22318de6dcd43e1
Nantag <piccioni.alfred@gmail.com>
2016-11-14 20:46:14 -0500

    Question 3 completed.

--- Project Euler/Question_3.cpp
+++ Project Euler/Question_3.cpp
@@ -1,3 +1,14 @@
+#include <stdio.h>
+
 int Question_3 () {
-	return 0;
+	long long int target = 600851475143;
+	int prime = 2;
+	while (target > 1) {
+		if (!(target % prime)) {
+			target /= prime;
+		} else {
+			prime++;
+		}
+	}
+	return prime;
 }
