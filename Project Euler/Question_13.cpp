cfab59173f5a8f878a585a64b41c39ce92d90116
Nantag <piccioni.alfred@gmail.com>
2016-11-26 19:30:09 -0500

    Finished Question 13. Note that Question 13 was solved after  Question 12, but IO problems delayed it.

--- Project Euler/Question_13.cpp
+++ Project Euler/Question_13.cpp
@@ -1,5 +1,37 @@
+using namespace std;
+#include <string>
+#include <iostream>
+#include <fstream>
 #include "Question_13.h"
 
+int Count_Digits (unsigned long long target) {
+	int count = 0;
+	while (target != 0) {
+		target /= 10;
+		count++;
+	}
+	return count;
+}
+
 long long Question_13 () {
-	return 0;
+
+	int FIRST_NUMBERS = 10;
+
+	ifstream file;
+	file.open ("./Question_13_Numbers.txt");
+	string input;
+	unsigned long long answer = 0;
+
+	while (file >> input) {
+		unsigned long long in_Ans = stoll (input.substr (0, FIRST_NUMBERS + 1));
+		answer += in_Ans;
+	}
+
+	int Digits = Count_Digits (answer);
+	while (Digits > FIRST_NUMBERS) {
+		answer /= 10;
+		Digits--;
+	}
+
+	return answer;
 }
