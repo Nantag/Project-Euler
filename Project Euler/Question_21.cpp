75b1eee756edba099ca82d4a59d330e0409b9d48
Nantag <piccioni.alfred@gmail.com>
2016-12-14 10:03:26 -0500

    Finished Question 21 up to proper standards.

--- Project Euler/Question_21.cpp
+++ Project Euler/Question_21.cpp
@@ -5,28 +5,57 @@ using namespace std;
 namespace Question_21 {
 
 	int Proper_Divisors_Sum (int target) {
-		int limit = (target / 2) + 1;
-		int sum = 0;
-		for (int i = 1; i < limit; i++) {
-			if (!(target % i)) {
-				sum += i;
+
+		int target_backup = target;
+		int sum = 1;
+		int power = 0;
+
+		// Finds the prime factors of the target.
+		for (int i = 2;target > 1; i++) {
+			while (!(target % i)) {
+				target /= i;
+				power++;
 			}
+
+			// Finds the divisors using the algorithm detailed in the header file.
+			if (power != 0) {
+				if (power == 1) {
+					sum *= (i + 1);
+				} else {
+					sum *= ((pow(i, power + 1) - 1) / (i - 1));
+				}
+				power = 0;
+			}
+
 		}
+
+		// Remove the only non-proper divisor from the sum.
+		sum -= target_backup;
+
 		return sum;
 	}
 
 	int Answer () {
+
+		int AMICABLE_UP_TO = 10000;
+
 		int sum = 0;
-		for (int i = 2; i < 10000; i++) {
+		for (int i = 2; i < AMICABLE_UP_TO; i++) {
+
+			// Checks if a given number is amicable.
 			int a = Proper_Divisors_Sum (i);
 			if (a > i) {
 				int b = Proper_Divisors_Sum (a);
+
+				// Sums the amicable numbers.
 				if (i == b) {
 					sum += a;
 					sum += b;
 					i = a + 1;
 				}
+
 			}
+
 		}
 		return sum;
 	}
--- Project Euler/Question_21.h
+++ Project Euler/Question_21.h
@@ -1,6 +1,20 @@
 namespace Question_21 {
 
+	//
 	int Proper_Divisors_Sum (int target);
 
-	int Answer ();
+	// Finds the sum of all amicable numbers below 10,000.
+	int Answer();
+
+	// Finding amicable numbers is not particularly dificult algorithmically. 
+	// It is a simple matter of comparing the sum of proper divisors of each number.
+	// Finding the sum of proper divisors, on the other hand, is more interesting. The brute force method works fine, but is slow.
+	// An alternative algorithm is used, based on properties of the divisor function.
+	// ALGORITHM:
+	//		1. Find the prime factorization of the number.
+	//		2. Sum the prime factors using the following formula:
+	//			Pi (n=0 -> i) [(F{n}^(P{n} + 1) - 1) / (F{n} - 1)], 
+	//				where i is the number of prime factors, F{n} is the nth prime factor, and P{n} is the power of the nth prime factor.
+	//		3. The sum is the sum of divisors.
+	//		4. sum - number = sum of proper divisors, as the number itself is the only improper divisor in the sum.
 }
